0a0b83c682d3ae3719cc9361b2d4fbc00774cf8f
kamal <kamhar-ngado>
2021-03-19 20:35:14 +0900

    buat hapus

--- masukanBarang.js
+++ masukanBarang.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("beli");
 var input = document.getElementById("inputUser");
 var ul = document.querySelector("ul");
-var hapus = document
+var hapus = document.getElementById("tidakJadiBeli");
 
 button.addEventListener("click", function addListAfterClick() {
   if (input.value.length > 0) {
@@ -14,6 +14,16 @@ button.addEventListener("click", function addListAfterClick() {
 
 input.addEventListener("keypress", function addListAfterKeypress(event) {
   if (input.value.length > 0 && event.keyCode === 13) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+  }
+});
+
+
+hapus.addEventListener("click", function addListAfterClick() {
+  if (input.value.length > 0) {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
