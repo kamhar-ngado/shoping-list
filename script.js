6fda2894fcb6e69ec5a517d15facbda45a8e9eff
kamal <kamhar-ngado>
2021-03-19 21:23:32 +0900

    perbaiki html dan javaScript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("beli");
 var input = document.getElementById("inputUser");
 var ul = document.querySelector("ul");
-var hapus = document.getElementById("tidakJadiBeli");
+
 
 button.addEventListener("click", function addListAfterClick() {
   if (input.value.length > 0) {
@@ -14,15 +14,6 @@ button.addEventListener("click", function addListAfterClick() {
 
 input.addEventListener("keypress", function (event) {
   if (input.value.length > 0 && event.keyCode === 13) {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-  }
-});
-
-hapus.addEventListener("click", function () {
-  if (input.value.length > 0) {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
