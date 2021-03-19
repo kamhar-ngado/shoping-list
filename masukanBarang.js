942960ccbc9322724c9312fb03a18e7e276ee4b9
kamal <kamhar-ngado>
2021-03-19 20:06:22 +0900

    delete fungsi inputLength

--- masukanBarang.js
+++ masukanBarang.js
@@ -2,12 +2,8 @@ var button = document.getElementById("beli");
 var input = document.getElementById("inputUser");
 var ul = document.querySelector("ul");
 
-function inputLenght() {
-  return input.value.length;
-}
-
 button.addEventListener("click", function addListAfterClick() {
-  if (inputLenght() > 0) {
+  if (input.value.length > 0) {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
@@ -15,7 +11,7 @@ button.addEventListener("click", function addListAfterClick() {
   }
 });
 input.addEventListener("keypress", function addListAfterKeypress(event) {
-  if (inputLenght() > 0 && event.keyCode === 13) {
+  if (input.value.length > 0 && event.keyCode === 13) {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
