e817c4b651f6cfd2d22a80d510195553527acbc4
kamal <kamhar-ngado>
2021-03-19 20:00:53 +0900

    delete createListElement

--- masukanBarang.js
+++ masukanBarang.js
@@ -6,20 +6,19 @@ function inputLenght() {
   return input.value.length;
 }
 
-function createListElement() {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
-}
-
 button.addEventListener("click", function addListAfterClick() {
   if (inputLenght() > 0) {
-    createListElement();
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
   }
 });
 input.addEventListener("keypress", function addListAfterKeypress(event) {
   if (inputLenght() > 0 && event.keyCode === 13) {
-    createListElement();
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
   }
 });
